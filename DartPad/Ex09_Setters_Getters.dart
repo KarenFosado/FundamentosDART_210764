1c862eac89e88278675b6ae0bf76de5192b18b25
alyn.fosado <210764@utxicotepec.edu.mx>
2024-10-04 11:50:43 -0600

    Creacion de practica 12

--- DartPad/Ex09_Setters_Getters.dart
+++ DartPad/Ex09_Setters_Getters.dart
@@ -1,40 +1,36 @@
-void main() {
-  final mySquare = Square(side: 18);
-  
-  // Usamos el setter aquí para asignar un nuevo valor
-  try {
-    mySquare.side = -32; // Esto debería lanzar una excepción
-  } catch (e) {
-    print(e); // Imprime el error de validación del setter
-  }
+void main() 
+{
+  final mySquare = Square(side:18);
 
-  print("""Lado del Cuadrado: ${mySquare.side}
-Área que ocupa: ${mySquare.calculateArea()}""");
+
+  print("""Lado del Cuadrado: ${mySquare._side}
+  Área que ocupa: ${mySquare.calculateArea()}""");
+  mySquare.side = -32;
+
 }
 
 class Square {
-  double _side;
+  double _side; // Formula para calcular el Área lado * lado
+  Square({required double side})
+    :_side=side;
 
-  // Constructor con inicialización adecuada del campo _side
-  Square({required double side}) : _side = side;
-
-  // Getter para el área
-  double get area {
-    return _side * _side;
+  double get area
+  {
+    return _side*_side;
   }
 
-  // Setter con validación
-  set side(double value) {
-    print("Asignando un nuevo valor al lado: $value");
-    if (value < 0) throw 'El valor del lado debe ser mayor a 0.';
-    _side = value;
+  set side(double value)
+  {
+    print("Asignando un nuevo valor al lado : $value");
+    if(value <0 ) throw 'El valor del lado debe ser mayor a 0.';
+
+    _side= value;
   }
 
-  // Getter para el lado (para que sea accesible externamente)
-  double get side => _side;
-
-  // Método para calcular el área usando el valor de side
-  double calculateArea() {
-    return _side * _side;
+  double calculateArea()
+  {
+    return _side*_side;
   }
 }
+
+
